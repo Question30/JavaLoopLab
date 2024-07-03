4b0c05d8ff7042375ec041c6686fc96fc70504eb
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-07-03 09:43:55 -0400

    refactored GCD

--- src/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor.java
@@ -1,16 +1,24 @@
+import java.util.Scanner;
+
 public class GreatestCommonDivisor {
     public static void main(String[] args) {
         //Find the greatest common divisor
-        System.out.println(GCD(2, 4));
-        System.out.println(GCD(16, 24));
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter two positive integers: ");
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+
+
+
+        System.out.println(GCD(n1,n2));
 
     }
 
     public static int GCD(int n1, int n2){
-        int largest = Math.max(n1, n2);
+        int smallest = Math.min(n1, n2);
         int gcd = 1;
 
-        for (int i = 1; i < largest; i++){
+        for (int i = 2; i < smallest; i++){
             if(n1 % i == 0 && n2 % i == 0){
                 if(i > gcd){
                     gcd = i;
