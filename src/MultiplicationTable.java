b5d049d1f86b1a8137bcf1a01dc17ad56c0a5994
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-07-03 09:37:42 -0400

    fixed multiplication table

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -5,7 +5,7 @@ public class MultiplicationTable {
 
         for (int i = 1; i <= 12 ; i++){
             for(int j = 1; j <= 12; j ++){
-                System.out.print(i * j);
+                System.out.print(i * j + "\t");
             }
             System.out.println();
         }
